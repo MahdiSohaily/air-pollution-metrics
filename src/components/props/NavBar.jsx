e1dc0e6201bd43fa1abb207e34e29919e0fad677
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-28 10:53:55 +0430

    Update navbar styles

--- src/components/props/NavBar.jsx
+++ src/components/props/NavBar.jsx
@@ -1,44 +1,39 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
-import Logo from '../../imgs/logo.PNG';
+import { NavLink } from 'react-router-dom';
+import Logo from '../../imgs/logo.jpg';
 import '../../styles/nav-banner.css';
 
 function NavBar() {
   return (
-    <div>
-      <Navbar bg="light" expand="lg">
-        <Container fluid className="container">
-          <Navbar.Brand>
-            <Link to="/">
-              <img src={Logo} alt="Site logo" />
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0 nav-links"
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-              <Link to="/">
-                <h5 className="nav-link">Home</h5>
-              </Link>
-              <Link to="/contact">
-                <h5 className="nav-link">Contact Us</h5>
-              </Link>
-            </Nav>
-            <Form className="d-flex">
-              <Button variant="outline-success">Sign-Up for free</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <header>
+      <nav className="navBar d-flex wrapper">
+        <div className="navBrand d-flex">
+          <img
+            width={50}
+            height={50}
+            className="logo"
+            src={Logo}
+            alt="Site logo"
+          />
+        </div>
+        <ul className="menuNav d-flex">
+          <li className="nav-link">
+            <NavLink className="text-secondary-400" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-link">
+            <NavLink className="text-secondary-400" to="/contact">
+              Contact Us
+            </NavLink>
+          </li>
+          <li className="nav-link">
+            <button className="action-button free" type="button">
+              Sign-Up for free
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
