dcdba0dd1e320f8cebc4d7c51694890a328c97b6
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-28 21:15:33 +0430

    Update contact page style

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -6,14 +6,17 @@ function Contact() {
   return (
     <div>
       <NavBar />
-      <div className="contact-info shadow p-3 mb-5 bg-white">
-        <h1>Contact Information</h1>
-        <p className="text-secondary">
-          Contact us via mail (mahdi.sohaily4030@gmail.com)
-        </p>
-        <p>
-          Or click on any of the social media links below to reach me directly.
-        </p>
+      <div className="contact-container bg-main">
+        <div>
+          <h1 className="text-neutral-100">Contact Information</h1>
+          <p className="text-neutral-100">
+            Contact us via mail (mahdi.sohaily4030@gmail.com)
+          </p>
+          <p className="text-neutral-100">
+            Or click on any of the social media links below to reach me
+            directly.
+          </p>
+        </div>
       </div>
       <Footer />
     </div>
