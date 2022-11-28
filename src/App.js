92bae1aac38dbc1a90a11ee69bb0bf4517977def
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-28 09:53:41 +0430

    Add  project's routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Pages/Home';
+import Details from './Components/Pages/Details';
+import Contact from './Components/Pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:country" element={<Details />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
