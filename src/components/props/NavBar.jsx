faffedbe87a0527b784f504636adac6de39a05cb
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-28 17:09:00 +0430

    Update nav style

--- src/components/props/NavBar.jsx
+++ src/components/props/NavBar.jsx
@@ -1,33 +1,42 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import Logo from '../../imgs/logo.jpg';
 import '../../styles/nav-banner.css';
 
 function NavBar() {
   return (
-    <header className="bg-dodgerblue">
-      <nav className="d-flex navBar">
-        <div className="navBrand">
-          <img
-            width={50}
-            height={50}
-            className="logo"
-            src={Logo}
-            alt="Site logo"
-          />
-        </div>
-        <div className="d-flex navMenu">
-          <NavLink className="text-neutral-100" to="/">
-            Home
-          </NavLink>
-          <NavLink className="text-neutral-100" to="/contact">
-            Contact Us
-          </NavLink>
-          <button className="action-button" type="button">
-            Sign-Up for free
-          </button>
-        </div>
-      </nav>
-    </header>
+    <>
+      <header className="bg-dodgerblue">
+        <nav className="d-flex navBar">
+          <div className="navBrand">
+            <img
+              width={50}
+              height={50}
+              className="logo"
+              src={Logo}
+              alt="Site logo"
+            />
+          </div>
+          <div className="navMenu">
+            <ul>
+              <li className="nav-link">
+                <NavLink className="text-neutral-100" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-link">
+                <NavLink className="text-neutral-100" to="/contact">
+                  Contact Us
+                </NavLink>
+              </li>
+            </ul>
+            <button className="action-button" type="button">
+              Sign-Up for free
+            </button>
+          </div>
+        </nav>
+      </header>
+      <Outlet />
+    </>
   );
 }
 
